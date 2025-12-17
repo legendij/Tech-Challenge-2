6d1e34491bf54380d7663e25455001bb7c4d156e
legendij <johnsonisaiah91@gmail.com>
2025-12-16 21:47:47 -0500

    Test: change response to confirm Jenkins redeploy

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.status(200).send("OK"));
+app.get("/", (req, res) => res.status(200).send("OK - deployed by Jenkins ✅"));
 app.get("/health", (req, res) => res.status(200).json({ status: "healthy" }));
 
 app.listen(PORT, "0.0.0.0", () => {
